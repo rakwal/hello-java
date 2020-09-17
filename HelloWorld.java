1c851f302198c3c7087e488971fca3518f2fd9db
rakwal <39078909+rakwal@users.noreply.github.com>
2020-09-17 16:57:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
-        System.out.println("Hello World, DevOps has arrived Sonal !");
+        System.out.println("Hello World, DevOps has arrived Sonal Rakwal!");
     }
 
 }
